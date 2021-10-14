8284d2e17c456d3f0d7ecf0f208831943261838b
Minjjeyewv <mineev.rem@yandex.ru>
2021-10-14 21:06:15 +0400

    add sart files

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () =>{
+    return(
+        <header className="header">
+            <img src="https://images.theconversation.com/files/93616/original/image-20150902-6700-t2axrz.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1000&fit=clip"/>
+        </header>
+    )
+}
+export default Header;
